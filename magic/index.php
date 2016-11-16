75f3e186b495f67770480d7e99ea9c27c3584e39
SleekPanther <noahpatullo@gmail.com>
2016-11-16 18:32:28 -0500

    value submit & save (no validate)

--- magic/index.php
+++ magic/index.php
@@ -7,27 +7,29 @@ $tabIndex=1;
 $title='title';			//variables to hold form input
 $runtime='run';
 $rating='pg';
+$releaseDate='hidden';
 $display='dis';
-$releaseDate='2day';
 $director='direc';
 
 $titleError=false;		//error variables for form input validation
 $runtimeError=false;
 $ratingError=false;
+//$releaseDateError=false;
 $displayError=false;
-$releaseDateError=false;
 $directorError=false;
 
 $errorMsg=array();
 
-$ratings=array("G","PG","PG-13","R","Not Rated","NC-17");
+$ratings=array("G","PG","PG-13","R","Not Rated","NC-17");	//only valid options for MPAA ratings listbox
+$displayOptions=array('Hidden', 'Current', 'Coming Soon');		//only valid options 4 display listbox
 
 if(isset($_POST['btnAddMovie'])){
+	echo "<pre>".print_r($_POST)."</pre>";	//prints extra 1 from print_r saying "true"
 	$title=htmlentities($_POST['txtMovieTitle'], ENT_QUOTES, "UTF-8");
 	$runtime=htmlentities($_POST['txtRuntime'], ENT_QUOTES, "UTF-8");
 	$rating=htmlentities($_POST['lstRating'], ENT_QUOTES, "UTF-8");
+	$releaseDate=htmlentities($_POST['datReleaseDate'], ENT_QUOTES, "UTF-8");
 	$display=htmlentities($_POST['lstDisplay'], ENT_QUOTES, "UTF-8");
-	$releaseDate=htmlentities($_POST['datDate'], ENT_QUOTES, "UTF-8");
 	$director=htmlentities($_POST['txtDirector'], ENT_QUOTES, "UTF-8");
 
 	//if rating not emptu
@@ -56,10 +58,23 @@ if(isset($_POST['btnAddMovie'])){
 			
 			echo "\t\t\t<label for='lstRating'>Rating</label>\n";
 			echo "\t\t\t<select id='lstRating' name='lstRating' tabindex='".$tabIndex++."' >\n";
-				foreach($ratings as $rating){
-					echo "\t\t\t\t<option value='".$rating;
-					//print selected
-					echo "'>".$rating."</option>\n";
+				foreach($ratings as $oneRating){
+					echo "\t\t\t\t<option value='".$oneRating."'";
+					if($oneRating==$rating){ echo ' selected ';}
+					echo ">".$oneRating."</option>\n";
+				}
+			echo "\t\t\t</select><br>\n";
+
+			echo "<label for='datReleateDate'>Release Date</label>\n";
+			echo "\t\t\t<input type='date' name='datReleaseDate' id='datReleateDate' tabindex='".$tabIndex++."' value='".$releaseDate."'";
+			echo "><br>\n";
+
+			echo "\t\t\t<label for='lstDisplay'>Visibility (display to public or not)</label>\n";
+			echo "\t\t\t<select id='lstDisplay' name='lstDisplay' tabindex='".$tabIndex++."' >\n";
+				foreach($displayOptions as $option){
+					echo "\t\t\t\t<option value='".$option."'";
+					if($option==$display){echo ' selected ';}
+					echo ">".$option."</option>\n";
 				}
 			echo "\t\t\t</select><br>\n";
 
