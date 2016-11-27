53616cc136d60b39628a7d8a533ac0332cc220d7
SleekPanther <noahpatullo@gmail.com>
2016-11-27 18:54:31 -0500

    header css

--- php/header.php
+++ php/header.php
@@ -12,8 +12,8 @@
 				<img src="<?php echo $upFolderPlaceholder;?>images/logos/header/majestic-logo.jpg" alt="Majestic 10">
 				<img src="<?php echo $upFolderPlaceholder;?>images/logos/header/palace9-logo.jpg" alt="Palace 9">
 				<div class='socialMediaPics'>
-					<a href="http://www.facebook.com/pages/Burlington-VT/Merrills-Roxy-Cinema-Burlington-Vermont/102778324627"><img src="<?php echo $upFolderPlaceholder;?>images/logos/header/facebook.jpg" alt="Palace 9"></a>
-					<a href="https://twitter.com/#!/Merrills_Roxy"><img src="<?php echo $upFolderPlaceholder;?>images/logos/header/twitter.jpg" alt="Palace 9"></a>
+					<a href="http://www.facebook.com/pages/Burlington-VT/Merrills-Roxy-Cinema-Burlington-Vermont/102778324627"><img src="<?php echo $upFolderPlaceholder;?>images/logos/header/facebook.png" alt="Facebook"></a>
+					<a href="https://twitter.com/Merrills_Roxy"><img src="<?php echo $upFolderPlaceholder;?>images/logos/header/twitter.png" alt="Twitter"></a>
 				</div>
 			</figure>
 		</section>
