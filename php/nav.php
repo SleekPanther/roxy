c0b56e94a1d91d3d09fb104e94f8dc70a208442c
SleekPanther <noahpatullo@gmail.com>
2016-11-28 14:34:07 -0500

    nav & css

--- php/nav.php
+++ php/nav.php
@@ -6,10 +6,16 @@
 
     if username=in username array (npatullo, rerickso, yling19, ), then show extra link -->
         <a href="<?php echo $upFolderPlaceholder;?>index.php">Home</a>
+        <a href="<?php echo $upFolderPlaceholder;?>synopses.php">Synopses</a>
+        <a href="<?php echo $upFolderPlaceholder;?>reviews.php">Reviews</a>
+        <a href="<?php echo $upFolderPlaceholder;?>soon.php">Coming Soon</a>
+        <a href="<?php echo $upFolderPlaceholder;?>prices.php">Prices</a>
+        <a href="<?php echo $upFolderPlaceholder;?>menu.php">Menu?</a>
+        <a href="<?php echo $upFolderPlaceholder;?>about.php">About</a>
     	<?php
     	if (in_array($netId, $validAdmins)){		//only print admin links if they're an admin
 		?>
-        	<a href="<?php echo $upFolderPlaceholder;?>magic/index.php">Add/Edit Movies (admin)</a>
+        	<a href="<?php echo $upFolderPlaceholder;?>magic/index.php">Admin</a>
         <?php
 	    }
 	    ?>
