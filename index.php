ae32931772d54b27a6a5b1a1c66f874fe0587c2c
SleekPanther <noahpatullo@gmail.com>
2016-11-27 21:29:11 -0500

    selects showtimes (not pretty)

--- index.php
+++ index.php
@@ -4,11 +4,6 @@ include	"php/top.php";
 ?>
 	<article>
 		<?php
-		// $query="SELECT pmkMovieId, fldTitle, fldRuntime, fldRating, fldReleaseDate, fldDisplay, fldDirector,
-		//  fldImgFilename FROM tblMovies
-		//  JOIN tblPictures ON pmkMovieId=fnkMovieId
-		//  WHERE ( (fldDisplay='Current' OR fldDisplay='Coming Soon') AND (fldReleaseDate<=CURDATE()) )
-		//   ORDER BY fldReleaseDate DESC";
 
 		//get current movies. & only if the release date is BEFORE today
 		$query="SELECT pmkMovieId, fldTitle, fldRuntime, fldRating, fldReleaseDate, fldDisplay, fldDirector,
@@ -19,9 +14,41 @@ include	"php/top.php";
 
 		$data=array('Current','Coming Soon');
 		$movies=$thisDatabaseReader->select($query,$data,1,3,0,1);
-		echo "<pre>";
-		print_r($movies);
-		echo "</pre>";
+		
+
+		// $counter=0;
+		// for($date=date("Y-m-d", strtotime('last sunday')); $counter<1; $date2=date("Y-m-d", strtotime($date.'+7 days'))){
+		$date=date("Y-m-d", strtotime('last friday'));
+		$date2=date("Y-m-d", strtotime($date.'+6 days'));
+		foreach($movies as $movie){
+			echo "\t\t<article class='movie'>\n";
+			echo "\t\t\t<h2>".$movie['fldTitle']."</h2>\n";
+			echo "\t\t\t<section class='showtimesDetail'>";
+
+			// $query="SELECT fldHour, fldMinute, fldMeridian, fldShowtimePosts, fldShowtimeExpires, fldDimension FROM tblShowtimes
+			//  WHERE ((fldShowtimePosts >= CAST('2016-11-25' AS DATE) ) AND (fldShowtimeExpires <= CAST('2016-11-27' AS DATE) ) AND  fnkMovieId=41)
+			//   ORDER BY fldMeridian, fldHour, fldMinute";
+			$query="SELECT fldHour, fldMinute, fldMeridian, fldShowtimePosts, fldShowtimeExpires, fldDimension FROM tblShowtimes
+			 WHERE ((fldShowtimePosts >= ? ) AND (fldShowtimeExpires <= ? ) AND  fnkMovieId=?)
+			  ORDER BY fldMeridian, fldHour, fldMinute";
+			$data=array($date,$date2,$movie['pmkMovieId']);
+			$thisDatabaseReader->testquery($query,$data,1,3,0,2);
+			$showtimes=$thisDatabaseReader->select($query,$data,1,3,0,2);
+			echo "<pre>";
+			print_r($showtimes);
+			echo "</pre>";
+			// foreach($showtimes as $showtime){
+			// 	echo "\t\t\t\t<p>"$showtime['fldHour'].":"$showtime['fldMinute']."<p>\n";
+			// }
+			echo "\t\t\t</section>";
+			echo "\t\t</article>\n";
+		}
+
+		// $a=0;
+		// for($date=date("Y-m-d", strtotime('last sunday'));$a<3;$date=date("Y-m-d", strtotime($date.'+7 days'))){
+		// 	echo $date."<br>";
+		// 	$a++;
+		// }
 		?>
 	</article>
 <?php
