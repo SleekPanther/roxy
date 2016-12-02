0f309ff6052bcbd17a5d6e77e03f4a601417e975
SleekPanther <noahpatullo@gmail.com>
2016-12-02 17:04:04 -0500

    list page notes

---
+++ list.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+include	"php/top.php";
+?>
+	<article class='movieContainer'>
+		<h1>Mailing List</h1>
+		sign up for our weekly mailing list to stay up date on showtimes, special events and upcoming movies
+		<?php
+
+		?>
+	</article>
+<?php
+include	"php/footer.php";
+?>
