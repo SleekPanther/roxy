8537f9ac18ec86f2dfe21a443aae4974985a7f9f
SleekPanther <noahpatullo@gmail.com>
2016-12-04 16:51:01 -0500

    prices cleanup

--- prices.php
+++ prices.php
@@ -2,7 +2,6 @@
 session_start();
 include	"php/top.php";
 ?>
-	</article>
 	<article class='movieContainer'>
 		<h1>Prices</h1>
 		<article class='halfCol articleBg'>
@@ -98,75 +97,6 @@ include	"php/top.php";
 				</table>
 			</section>
 		</article>
-
-		<?php
-
-		// for($repeat=true, $iterations=0, $date=date("Y-m-d", strtotime('last friday')); $repeat && $iterations<2; $iterations++, $date=date("Y-m-d", strtotime($date.' +7 days'))){
-		// 	$dateWeekEnds=date("Y-m-d", strtotime($date.'+6 days'));
-
-		// 	echo "<section class='tCent clear'>\n";
-		// 	echo "\t\t\t<h2>Showtimes for</h2>\n";
-		// 	echo "\t\t\t<h3>".dateSqlToNice($date)." to ".dateSqlToNice( $dateWeekEnds )."</h3>\n";
-		// 	echo "\t\t</section>\n";
-		// 	foreach($movies as $movie){
-		// 		echo "\t\t<article class='movie'>\n";
-		// 		$imgFile=$upFolderPlaceholder.'images/posters/ref/placeholder.png';
-		// 		if(file_exists($upFolderPlaceholder.'images/posters/'.$movie['fldImgFilename'])){
-		// 			$imgFile=$upFolderPlaceholder.'images/posters/'.$movie['fldImgFilename'];
-		// 		}
-		// 		echo "\t\t\t<figure><img alt='' src='".$imgFile."'></figure>\n";
-		// 		echo "\t\t\t<h2>".$movie['fldTitle']."</h2>\n";
-		// 		echo "\t\t\t<p>(".$movie['fldRating'].') '.runtimeToHoursMinutes($movie['fldRuntime'])."</p>\n";
-		// 		echo "\t\t\t<section class='showtimesDetail'>\n";
-
-		// 		// $query="SELECT fldHour, fldMinute, fldMeridian, fldShowtimePosts, fldShowtimeExpires, fldDimension FROM tblShowtimes
-		// 		//  WHERE ((fldShowtimePosts >= CAST('2016-11-25' AS DATE) ) AND (fldShowtimeExpires <= CAST('2016-11-27' AS DATE) ) AND  fnkMovieId=41)
-		// 		//   ORDER BY fldMeridian, fldHour, fldMinute";
-		// 		//WHERE ((fldShowtimePosts >= CAST('2016-11-25' AS DATE) ) AND (fldShowtimeExpires >= CURDATE() ) AND  fnkMovieId=41)
-		// 		$query="SELECT pmkShowtimeId, fldHour, fldMinute, fldMeridian, fldShowtimePosts, fldShowtimeExpires, fldDimension FROM tblShowtimes
-		// 		 WHERE ( (fldShowtimePosts <= ? ) AND (fldShowtimeExpires >= ? ) AND fnkMovieId=?)
-		// 		 ORDER BY fldDimension, fldMeridian, fldHour, fldMinute";
-		// 		$data=array($dateWeekEnds, $date, $movie['pmkMovieId']);
-		// 		$showtimes=$thisDatabaseReader->select($query,$data,1,3,0,2);
-
-		// 		$showtimes2D=array();
-		// 		$showtimes3D=array();
-		// 		foreach($showtimes as $showtime){
-		// 			//IF AM, then print AM
-					
-		// 			if($showtime['fldDimension']=='3D'){
-		// 				$showtimes3D[]= $showtime['fldHour'].":".leadingZeros($showtime['fldMinute'],2).' '.$showtime['fldDimension'];
-		// 			}else{
-		// 				$showtimes2D[]= $showtime['fldHour'].":".leadingZeros($showtime['fldMinute'],2);
-		// 			}
-		// 		}
-		// 		if(!empty($showtimes2D)){
-		// 			echo "\t\t\t\t<p>";
-		// 			foreach($showtimes2D as $time){
-		// 				echo $time.' ';
-		// 			}
-		// 			echo "</p>\n";
-		// 		}
-
-		// 		if(!empty($showtimes3D)){
-		// 			echo "\t\t\t\t<p>";
-		// 			foreach($showtimes3D as $time){
-		// 				echo $time." &nbsp;";
-		// 			}
-		// 			echo "</p>\n";
-		// 		}
-
-		// 		echo "\t\t\t</section>\n";
-		// 		echo "\t\t</article>\n";
-		// 	}
-
-		// 	$repeat=false;	//set false after each iteration
-		// 	if(date("l", strtotime('today')) == 'Thursday'){		//check if it's 1 day before a new week & then we want to say YES DO REPEAT (to print next week's schedule)
-		//         $repeat=true;
-		//     }
-		// }
-
-		?>
 	</article>
 <?php
 include	$upFolderPlaceholder."php/footer.php";
