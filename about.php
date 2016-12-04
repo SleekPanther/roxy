8bbac8787dcded8683a69aa40dfab8fed17f9975
SleekPanther <noahpatullo@gmail.com>
2016-12-04 16:59:14 -0500

    about

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include	"php/top.php";
+?>
+	<article class='movieContainer'>
+		<h1>About Us</h1>
+		<article class='articleBg'>
+		<figure id='imgRoxyAbout'><img src="<?php echo $upFolderPlaceholder.'images/about/roxy-about.jpg'; ?>" alt="Merrill's Roxy Cinema"></figure>
+		<p>Merrill's Roxy Cinemas has been a fixture in Downtown Burlington since 2003.  From day one we have specialized in bringing an eclectic mix of films with a strong focus on smaller and thought provoking features.  We are locally owned by the Merrill Theatre group who has had a strong presence in Downtown Burlington for many years!</p>
+		<p>Merrill Theatres has embraced the innovations in comfort, picture and sound at first by installing comfortable luxury rocker chairs and digital sound in our auditoriums.  In March of 2013 we will take the bigger plunge of converting our projection booth to total a digital presentation.  We are no stranger to Digital Presentation as Majestic 10--our Flagship Stadium Seating Theatre in Williston--was the first to bring Digital projection, Digital 3D and 7.1 Sound to Vermont!</p>
+		<p>The Merrill Theatre Corporation recently acquired The Palace 9 in South Burlington and we are excited with the opportunities that will bring allowing us to expand our unique programming to the Greater Burlington Market.</p>
+		<p>Roxy Cinemas was voted Best Movie Theatre nine years straight by the <strong>Seven Daysies Annual Guide to Reader's Picks</strong>. This weekly Vermont newspaper is popular as an Alternative source for local news, views and culture</p>
+		<p>We are surrounded by several Burlington colleges and it only made sense to offer students reduced prices on tickets.  Our student discount runs seven days a week for both matinee and evening shows!  We are also in our 3rd year partnering with  the University of Vermont for it's CATS AT THE MOVIES which offers free Roxy Movie Tickets to it's students. <a href="http://uvmbored.com/cats-at-the-movies/">Click here for more information</p>
+		</article>
+	</article>
+<?php
+include	$upFolderPlaceholder."php/footer.php";
+?>
