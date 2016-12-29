139c8bee4b973f2639db628763d9736eabe0c6cb
SleekPanther <macrobyte2@gmail.com>
2016-12-29 10:08:23 -0500

    fixed bug in sql query logic (movies with no showtimes still appeared)

--- index.php
+++ index.php
@@ -3,28 +3,29 @@ include	"php/top.php";
 ?>
 	<article class='movieContainer'>
 		<?php
-		//get current movies & only if the release date is BEFORE today
-		//Do a INNER JOIN on tblShowtimes to ensure a movie is actually playing. This gets seemingly duplicate results since this 1st query just want general info about a movie (not the showtime) but tblShowtimes JOIN gets a row for each showtime. SO ADD DISTINCT to "only get movies that have showtimes & just the info about the movie itself, not showtimes yet"
-		//doesn't completely solve issue since expired showtimes won't display
-		$query="SELECT DISTINCT pmkMovieId, fldTitle, fldRuntime, fldRating, fldReleaseDate, fldDisplay, fldDirector,
-		 fldImgFilename FROM tblMovies
-		 LEFT JOIN tblPictures ON pmkMovieId=tblPictures.fnkMovieId
-		 JOIN tblShowtimes ON pmkmovieId=tblShowtimes.fnkMovieId
-		 WHERE ( (fldDisplay=? ) AND (fldReleaseDate<=CURDATE()) )
-		 ORDER BY fldReleaseDate DESC";
-		$data=array('Current');
-		$movies=$thisDatabaseReader->select($query,$data,1,2,0,1);
-
 		//$repeat controls if print 1 or 2 copies of showtimes. Start true so always happens once, then print next week's schedule if it's thursday
-		//$date is a friday, the beginning of a 'week'
+		//$dateWeekStarts is a friday, the beginning of a 'week'
 		//iterates while $repeat==true & only 1 or 2 iterations (no more)
 		//increments increase $iterations & add 7 days to the week's start date
-		for($repeat=true, $iterations=0, $date=date("Y-m-d", strtotime('last friday', strtotime('tomorrow')) ); $repeat && $iterations<2; $iterations++, $date=date("Y-m-d", strtotime($date.' +7 days'))){
-			$dateWeekEnds=date("Y-m-d", strtotime($date.'+6 days'));
+		for($repeat=true, $iterations=0, $dateWeekStarts=date("Y-m-d", strtotime('last friday', strtotime('tomorrow')) ); $repeat && $iterations<2; $iterations++, $dateWeekStarts=date("Y-m-d", strtotime($dateWeekStarts.' +7 days'))){
+			$dateWeekEnds=date("Y-m-d", strtotime($dateWeekStarts.'+6 days'));
+
+			//get current movies: Left join pictures in case non-existant. Inner join showtimes 
+			//where conditions: 1)must be 'Current', 2)released before the end of the week 3)showtimes exist 3a)showtime posts before the end of the week 3b)showtime expires AFTER the start of the week
+			//DISTINCT might be optional, but leave it just in case
+			$query="SELECT DISTINCT pmkMovieId, fldTitle, fldRuntime, fldRating, fldReleaseDate, fldDisplay, fldDirector,
+			 fldImgFilename FROM tblMovies
+			 LEFT JOIN tblPictures ON pmkMovieId=tblPictures.fnkMovieId
+			 JOIN tblShowtimes ON pmkmovieId=tblShowtimes.fnkMovieId
+			 WHERE ( (fldDisplay=? ) AND (fldReleaseDate<=?) ) AND ( (fldShowtimePosts <= ? ) AND (fldShowtimeExpires >= ? ) )
+			 ORDER BY fldReleaseDate DESC";
+			$data=array('Current',$dateWeekEnds,$dateWeekEnds,$dateWeekStarts);
+			$movies=$thisDatabaseReader->select($query,$data,1,4,0,3);
+			$thisDatabaseReader->testquery($query,$data,1,4,0,3);
 
 			echo "<section class='tCent clear'>\n";
 			echo "\t\t\t<h2>Showtimes for</h2>\n";
-			echo "\t\t\t<h3>".dateSqlToNice($date)." to ".dateSqlToNice( $dateWeekEnds )."</h3>\n";
+			echo "\t\t\t<h3>".dateSqlToNice($dateWeekStarts)." to ".dateSqlToNice($dateWeekEnds)."</h3>\n";
 			echo "\t\t</section>\n";
 			foreach($movies as $movie){
 				echo "\t\t<div class='articleContainer halfCol'>\n";
@@ -41,7 +42,7 @@ include	"php/top.php";
 				$query="SELECT pmkShowtimeId, fldHour, fldMinute, fldMeridian, fldShowtimePosts, fldShowtimeExpires, fldDimension FROM tblShowtimes
 				 WHERE ( (fldShowtimePosts <= ? ) AND (fldShowtimeExpires >= ? ) AND fnkMovieId=?)
 				 ORDER BY fldDimension, fldMeridian, fldHour, fldMinute";
-				$data=array($dateWeekEnds, $date, $movie['pmkMovieId']);
+				$data=array($dateWeekEnds, $dateWeekStarts, $movie['pmkMovieId']);
 				$showtimes=$thisDatabaseReader->select($query,$data,1,3,0,2);
 
 				$showtimesDimensionArray=array(array(),array());	//0th index=2D showtimes, 1st=3D showtimes
